4bc55a76c820faac82aa056fd10fd583bc9ee420
samangan <harry.hull1@gmail.com>
2013-12-09 15:00:30 -0600

    Fixed karma.conf.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('dailyFeelingTrackerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $localStorage) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    // TODO: remove test
+    $scope.$storage = $localStorage.$default({
+      x: 42
+    });
   });
