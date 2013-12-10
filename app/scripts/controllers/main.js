a64587e636943a9c5ce8c8e99c96ec305c29d021
Samangan <harry.hull1@gmail.com>
2013-12-09 21:09:36 -0600

    removed yo test stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,5 @@
 
 angular.module('dailyFeelingTrackerApp')
   .controller('MainCtrl', function ($scope, $localStorage) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    // TODO: remove test
-    $scope.$storage = $localStorage.$default({
-      x: 42
-    });
+    
   });
