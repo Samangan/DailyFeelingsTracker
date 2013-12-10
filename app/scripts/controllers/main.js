0ba1a737fbe844e329d317006d12ecae7b79da93
samangan <harry.hull1@gmail.com>
2013-12-10 15:16:49 -0600

    refactored getFirstAndLastDayOfTheWeek() to getFirstDayOfTheWeek( ) because I only need the first day of the week to grab the entire week

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,8 +6,7 @@ app.factory('ThisWeek', function () {
     var Week = function () {
 
 
-      //TODO: I only need to return the 1st day of the week. (refactor)
-      this.getFirstAndLastDayOfCurrentWeek = function (currentDate) {
+      this.getFirstDayOfTheWeek = function (currentDate) {
         // currentDate is only for testing that this function works.
         // it is an optional parameter
         if (!currentDate) {
@@ -17,30 +16,27 @@ app.factory('ThisWeek', function () {
         var previousSunday = new Date(currentDate);
         previousSunday.setDate(previousSunday.getDate() - previousSunday.getDay());
 
-        var thisSaturday = new Date(currentDate);
-        thisSaturday.setDate(thisSaturday.getDate() - thisSaturday.getDay() + 6);
-
-        return [previousSunday, thisSaturday];
+        return previousSunday;
       };
        
       this.days = [];
 
       // Generate this week
-      var firstAndLastDaysOfThisWeek = this.getFirstAndLastDayOfCurrentWeek();
+      var firstDayOfTheCurrentWeek = this.getFirstDayOfTheWeek();
 
       // Push the first day
       this.days.push({
         id: 0,
-        date: firstAndLastDaysOfThisWeek[0].toString(),
+        date: firstDayOfTheCurrentWeek.toString(),
         moodRanking: 5,
         tags: ['tired', 'drank too much'],
         currentDay: false
       });
 
-      for (var i = 1; i <= 5; i++) {
-        var day = new Date(firstAndLastDaysOfThisWeek[0]);
+      for (var i = 1; i < 7; i++) {
+        var day = new Date(firstDayOfTheCurrentWeek);
         day.setHours(0,0,0,0);
-        day.setDate(firstAndLastDaysOfThisWeek[0].getDate() + i);
+        day.setDate(firstDayOfTheCurrentWeek.getDate() + i);
 
         this.days.push({
           id: i,
@@ -50,14 +46,6 @@ app.factory('ThisWeek', function () {
           currentDay: false
         });
       }
-
-      this.days.push({
-        id: 6,
-        date: firstAndLastDaysOfThisWeek[1].toString(),
-        moodRanking: 5,
-        tags: ['tired', 'drank too much'],
-        currentDay: false
-      });
 
       //TODO: set current day in this.days
 
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -18,30 +18,24 @@ describe('Controller: MainCtrl', function () {
 
   it('should return the actual first and last day of the week for the inputted day', function () {
     var currentDate = new Date(2013, 11, 9);
-    var startAndEndOfCurrentWeek = scope.thisWeek.getFirstAndLastDayOfCurrentWeek(currentDate);
+    var startOfCurrentWeek = scope.thisWeek.getFirstDayOfTheWeek(currentDate);
     var sunday = new Date(2013, 11, 8);
-    var saturday = new Date(2013, 11, 14);
-    expect(startAndEndOfCurrentWeek[0].getTime()).toBe(sunday.getTime());
-    expect(startAndEndOfCurrentWeek[1].getTime()).toBe(saturday.getTime());
+    expect(startOfCurrentWeek.getTime()).toBe(sunday.getTime());
   });
 
 
   it('should return the actual first and last day of the week even when the week spans multiple months', function () {
     var currentDate = new Date(2013, 9, 31);
-    var startAndEndOfCurrentWeek = scope.thisWeek.getFirstAndLastDayOfCurrentWeek(currentDate);
+    var startOfWeek = scope.thisWeek.getFirstDayOfTheWeek(currentDate);
     var sunday = new Date(2013, 9, 27);
-    var saturday = new Date(2013, 10, 2);
-    expect(startAndEndOfCurrentWeek[0].getTime()).toBe(sunday.getTime());
-    expect(startAndEndOfCurrentWeek[1].getTime()).toBe(saturday.getTime());
+    expect(startOfWeek.getTime()).toBe(sunday.getTime());
   });
 
   it('should return the actual first and last day of the week even when the week spans multiple years', function () {
     var currentDate = new Date(2013, 11, 31);
-    var startAndEndOfCurrentWeek = scope.thisWeek.getFirstAndLastDayOfCurrentWeek(currentDate);
+    var startOfWeek = scope.thisWeek.getFirstDayOfTheWeek(currentDate);
     var sunday = new Date(2013, 11, 29);
-    var saturday = new Date(2014, 0, 4);
-    expect(startAndEndOfCurrentWeek[0].getTime()).toBe(sunday.getTime());
-    expect(startAndEndOfCurrentWeek[1].getTime()).toBe(saturday.getTime());
+    expect(startOfWeek.getTime()).toBe(sunday.getTime());
   });
   
 });
