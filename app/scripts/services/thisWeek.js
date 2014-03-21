87b682ad2cbba21781e1923155c992cafa4b74f7
samangan <harry.hull1@gmail.com>
2014-03-21 15:34:50 -0500

    whitespace format

--- app/scripts/services/thisWeek.js
+++ app/scripts/services/thisWeek.js
@@ -4,89 +4,89 @@
 var app = angular.module('dailyFeelingTrackerApp.thisWeek');
 
 app.factory('ThisWeek', function ($localStorage) {
-    var Week = function ($localStorage) {
-
-      console.log($localStorage);
-
-      this.getFirstDayOfTheWeek = function (currentDate) {
-        // currentDate is only for testing that this function works.
-        // it is an optional parameter
-        if (!currentDate) {
-          currentDate = new Date();
-        }
-        currentDate.setHours(0,0,0,0);
-        var previousSunday = new Date(currentDate);
-        previousSunday.setDate(previousSunday.getDate() - previousSunday.getDay());
-
-        return previousSunday;
-      };
-
-      this.addTag = function (day) {
-        day.tags.push('--Enter Tag Name--');
-      };
-       
-      this.days = [];
-      var dayOfWeek = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ];
-
-      // Generate this week's data
-      var firstDayOfTheCurrentWeek = this.getFirstDayOfTheWeek();
-
-      for (var i = 0; i < 7; i++) {
-        var day = new Date(firstDayOfTheCurrentWeek);
-        day.setHours(0,0,0,0);
-        day.setDate(firstDayOfTheCurrentWeek.getDate() + i);
-
-        // Check if this date already exists in localStorage
-        // TODO: do not define a function within a loop.
-        // Externalize these inner functions outside of the loop.
-        if ($localStorage.days && $localStorage.days.filter(function (i) { if (i.date.toString() === day.toString()) {return true;} else {return false;}})) {
-          // Date already exists in localStorage
-          // Only write the id, and the currentDay fields (preserving all of the previous data data)
-          var prevDate = $localStorage.days.filter(function (i) { if (i.date.toString() === day.toString()) {return true;} else {return false;}});
-          console.log('exists, not totally overwritting');
-          this.days.push({
-            id: i,
-            day: dayOfWeek[new Date(prevDate.date).getDay()],
-            date: prevDate.date,
-            prettyDate: prevDate.prettyDate,
-            moodRanking: prevDate.moodRanking,
-            tags: prevDate.tags,
-            currentDay: false,
-            d3Date: prevDate.d3Date
-          });
-        } else {
-          // Date does not exist in localStorage
-          // Write everything about it.
-          console.log('doesnt exist, yet making');
-
-          this.days.push({
-            id: i,
-            day: dayOfWeek[day.getDay()],
-            date: day,
-            prettyDate: day.getMonth() + 1 + '/' + day.getDate() + '/' + day.getFullYear(),
-            moodRanking: i,
-            tags: ['worked too much', 'tired', 'really, really long tag test'],
-            currentDay: false,
-            d3Date: day.toISOString().slice(0, 10)
-          });
-        }
-
-        
+  var Week = function ($localStorage) {
+    
+    console.log($localStorage);
+    
+    this.getFirstDayOfTheWeek = function (currentDate) {
+      // currentDate is only for testing that this function works.
+      // it is an optional parameter
+      if (!currentDate) {
+        currentDate = new Date();
       }
-
-      // Set current day in this.days
-      this.days[new Date().getDay()].currentDay = true;
-      this.days[new Date().getDay()].day = 'Today'; // TODO: Do I want this?
-
+      currentDate.setHours(0,0,0,0);
+      var previousSunday = new Date(currentDate);
+      previousSunday.setDate(previousSunday.getDate() - previousSunday.getDay());
+      
+      return previousSunday;
     };
-
-    return Week;
-  });
+    
+    this.addTag = function (day) {
+      day.tags.push('--Enter Tag Name--');
+    };
+    
+    this.days = [];
+    var dayOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+    
+    // Generate this week's data
+    var firstDayOfTheCurrentWeek = this.getFirstDayOfTheWeek();
+    
+    for (var i = 0; i < 7; i++) {
+      var day = new Date(firstDayOfTheCurrentWeek);
+      day.setHours(0,0,0,0);
+      day.setDate(firstDayOfTheCurrentWeek.getDate() + i);
+      
+      // Check if this date already exists in localStorage
+      // TODO: do not define a function within a loop.
+      // Externalize these inner functions outside of the loop.
+      if ($localStorage.days && $localStorage.days.filter(function (i) { if (i.date.toString() === day.toString()) {return true;} else {return false;}})) {
+        // Date already exists in localStorage
+        // Only write the id, and the currentDay fields (preserving all of the previous data data)
+        var prevDate = $localStorage.days.filter(function (i) { if (i.date.toString() === day.toString()) {return true;} else {return false;}});
+        console.log('exists, not totally overwritting');
+        this.days.push({
+          id: i,
+          day: dayOfWeek[new Date(prevDate.date).getDay()],
+          date: prevDate.date,
+          prettyDate: prevDate.prettyDate,
+          moodRanking: prevDate.moodRanking,
+          tags: prevDate.tags,
+          currentDay: false,
+          d3Date: prevDate.d3Date
+        });
+      } else {
+        // Date does not exist in localStorage
+        // Write everything about it.
+        console.log('doesnt exist, yet making');
+        
+        this.days.push({
+          id: i,
+          day: dayOfWeek[day.getDay()],
+          date: day,
+          prettyDate: day.getMonth() + 1 + '/' + day.getDate() + '/' + day.getFullYear(),
+          moodRanking: i,
+          tags: ['worked too much', 'tired', 'really, really long tag test'],
+          currentDay: false,
+          d3Date: day.toISOString().slice(0, 10)
+        });
+      }
+      
+      
+    }
+    
+    // Set current day in this.days
+    this.days[new Date().getDay()].currentDay = true;
+    this.days[new Date().getDay()].day = 'Today'; // TODO: Do I want this?
+    
+  };
+  
+  return Week;
+});
