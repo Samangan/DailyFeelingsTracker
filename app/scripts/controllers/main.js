275965918becc85fd78c322ebd090769648c292c
samangan <harry.hull1@gmail.com>
2014-03-21 18:15:05 -0500

    Refactored how I handle storage of daily feeling data. I no longer separate the daily tags from the yearly data.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,43 +3,26 @@
 
 var app = angular.module('dailyFeelingTrackerApp.controller');
 
-
-// TODO: make a thisYear factory similar to thisWeek
-function updateYearData (currentWeek, yearData) {
-  console.log('updating year data');
-  for (var j = 0; j < currentWeek.length; j++) {
-    var dayInYear = yearData.filter(function (i) { if (i.date.toString() === currentWeek[j].toString()) {return true;} else {return false;}});
-    if (dayInYear) {
-      yearData[j] = currentWeek[j];
-    } else {
-      yearData.push(currentWeek[j]);
-    }
-  }
-  return yearData;
-}
-
-function MainCtrl ($scope, $localStorage, ThisWeek) {
+function MainCtrl ($scope, $localStorage, DayStorage) {
   //$localStorage.$reset();
+ 
+  
+  var dayStorage = new DayStorage($localStorage);
+  console.log($localStorage);
 
-  $scope.thisWeek = new ThisWeek($localStorage);
-
-
+  $scope.storage = $localStorage;
+  console.log($scope.storage);
+  
   $scope.editTag = function (day, index, value) {
     if(value.replace(/^\s+|\s+$/g, '')) {
       day.tags[index] = value;
     }
   };
-
-  $scope.thisWeekDays = $localStorage.$default({
-    days: $scope.thisWeek.days,
-    yearData: $scope.thisWeek.days
-  });
-
-  $scope.$watch('thisWeekDays.days', function(newVals, oldVals) {
-    // Update the yearly data 
-    $scope.thisWeekDays.yearData = updateYearData(newVals, $scope.thisWeekDays.yearData);
-  }, true);
-
+  
+  $scope.addTag = function (day) {
+    day.tags.push('--Enter Tag Name--');
+  }; 
+  
 }
 
 
